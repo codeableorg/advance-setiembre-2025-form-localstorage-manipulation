470afadc1ba4e65421fdc83ede4f5501f437bda0
codebit-00 <code.ciencia@gmail.com>
2025-10-01 18:05:30 -0500

    feat: add solution

--- app.js
+++ app.js
@@ -1,1 +1,30 @@
-// Happy code :)
+const mostrarDatos = (nombre, correo) => {
+  document.getElementById("savedName").textContent = "Nombre: " + nombre;
+  document.getElementById("savedEmail").textContent = "Correo: " + correo;
+};
+
+const checkLocalStorage = () => {
+  const nombreGuardado = localStorage.getItem("nombre");
+  const correoGuardado = localStorage.getItem("correo");
+  if (nombreGuardado && correoGuardado) {
+    mostrarDatos(nombreGuardado, correoGuardado);
+  }
+};
+
+document.getElementById("userForm").addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  const formData = new FormData(this);
+
+  const nombre = formData.get("name");
+  const correo = formData.get("email");
+
+  localStorage.setItem("nombre", nombre);
+  localStorage.setItem("correo", correo);
+
+  mostrarDatos(nombre, correo);
+
+  this.reset();
+});
+
+checkLocalStorage();
